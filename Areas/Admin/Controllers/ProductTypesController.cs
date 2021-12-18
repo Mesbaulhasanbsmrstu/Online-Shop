f5d635551e175c18c21552f58e9276445c0141cf
Mesbaulhasanbsmrstu <mesbaulhasanbsmrstu@gmail.com>
2021-12-18 15:09:14 +0600

    Add project files.

---
+++ Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Online_Shop.Data;
+using Online_Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Online_Shop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        ApplicationDbContext _db;
+
+        public ProductTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.ProdctTypes.ToList());
+        }
+
+        //Create get Action method
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Create post Action Method
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create(ProductTypes productTypes)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.ProdctTypes.Add(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productTypes);
+        }
+    }
+}
