479aff973630f17cfb986fa452083bde4865e4b9
Mesbaulhasanbsmrstu <mesbaulhasanbsmrstu@gmail.com>
2021-12-19 11:43:00 +0600

    edit/delete product types

--- Areas/Admin/Controllers/ProductTypesController.cs
+++ Areas/Admin/Controllers/ProductTypesController.cs
@@ -43,5 +43,109 @@ namespace Online_Shop.Areas.Admin.Controllers
             }
             return View(productTypes);
         }
+
+
+        //Create edit Action method
+        public ActionResult Edit(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+            var productType = _db.ProdctTypes.Find(id);
+            if(productType==null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+        //edit post Action Method
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Edit(ProductTypes productTypes)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productTypes);
+        }
+
+        //Create details Action method
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var productType = _db.ProdctTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+        //details post Action Method
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Details(ProductTypes productTypes)
+        {
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        //Create delete Action method
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var productType = _db.ProdctTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+        //delete post Action Method
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Delete(int id,ProductTypes productTypes)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+            if(id!=productTypes.Id)
+            {
+                return NotFound();
+            }
+            var productType = _db.ProdctTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Remove(productType);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productTypes);
+
+        }
+
     }
 }
