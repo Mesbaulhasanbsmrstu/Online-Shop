423766b0404b4f337d7384aaefe180fa2cc45bf7
Mesbaulhasanbsmrstu <mesbaulhasanbsmrstu@gmail.com>
2021-12-21 14:45:01 +0600

    add product table

---
+++ Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Online_Shop.Data;
+using Online_Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Online_Shop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class ProductController : Controller
+    {
+      private  ApplicationDbContext _db;
+        private IHostingEnvironment _he;
+
+
+        public ProductController(ApplicationDbContext db,IHostingEnvironment he)
+        {
+            _db = db;
+            _he = he;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.Products.Include(c=>c.ProductTypes).Include(f=>f.SpecialTag).ToList());
+        }
+
+        //Create get Action method
+        public IActionResult Create()
+        {
+            ViewData["productTypeId"] = new SelectList(_db.ProdctTypes.ToList(),"Id","ProductType");
+            ViewData["TagId"] = new SelectList(_db.SpecialTags.ToList(), "Id", "Tag");
+            return View();
+        }
+
+        //Create post Action Method
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create(Products products, IFormFile image)
+        {
+            if (ModelState.IsValid)
+            {
+                if(image!=null)
+                {
+                    var name = Path.Combine(_he.WebRootPath + "/images", Path.GetFileName(image.FileName));
+                    await image.CopyToAsync(new FileStream(name, FileMode.Create));
+                    products.Image = "images/" + image.FileName;
+                }
+                _db.Products.Add(products);
+                await _db.SaveChangesAsync();
+                TempData["save"] = "Product  has been saved";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(products);
+        }
+    }
+}
